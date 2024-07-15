c3efcb33d3602418d7353747b0844f39dfe45fc4
snehsuresh <snehsuresh002@gmail.com>
2024-07-15 16:27:46 -0500

    added images, dataset class and image_2_matrix class

---
+++ main.py
@@ -0,0 +1,34 @@
+import pathlib
+import cv2
+
+# Correct usage of pathlib.Path
+casc_path = pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_frontalface_default.xml"
+cascade_path = str(casc_path)
+
+
+#Build classifier
+clf = cv2.CascadeClassifier(cascade_path)
+
+
+camera = cv2.VideoCapture(0)
+
+while True:
+    _, frame = camera.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #grayscale image
+    faces = clf.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize = (30,30),
+        flags = cv2.CASCADE_SCALE_IMAGE
+    )
+
+    for (x,y, width, height) in faces:
+        cv2.rectangle(frame, (x,y), (x+width, y+height), (255,255,0), 2)
+
+    cv2.imshow("Faces", frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
